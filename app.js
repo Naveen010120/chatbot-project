574247eab93bfcc6303833f72f107358ffa0f473
Naveen010120 <raghuvamshinaveensingh@gmail.com>
2024-10-19 18:14:53 +0530

    committed files

---
+++ app.js
@@ -0,0 +1,19 @@
+function talk(){
+    var know = {
+    "hi" : "Hello, Naveen here ",
+    "hello":"hello, nice to have you  here",
+    "where are you from" : "Nirmal😊..",
+    "can you help me" : "yeah sure ,what's your problem",
+    "today's wheather report" : "today wheather is clear and sunny 😎😎",
+    "thank you": "its my pleaser",
+    "bye" : "Okay! Will meet soon.."
+    };
+    let user = document.getElementById('userBox').value;
+    let chatLog=document.getElementById('chatLog');
+    // chatLog.innerHTML = user + "<br>";
+    if (user in know) {
+    chatLog.innerHTML = know[user] + "<br>";
+    }else{
+   chatLog.innerHTML = "Sorry,I didn't understand <br>";
+    }
+    }
